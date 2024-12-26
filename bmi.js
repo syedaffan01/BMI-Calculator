86679350a20f9c3408c18829ccebb8bcba3ff0e3
syedaffan01 <148125787+syedaffan01@users.noreply.github.com>
2024-12-26 23:47:49 +0530

    Update bmi.js

--- bmi.js
+++ bmi.js
@@ -10,7 +10,7 @@ function bmiCalculator(weight, height) {
 
 /* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
 
-var bmi = bmiCalculator(65, 1.8); 
+var bmi = bmiCalculator(65, 1.8); .
 
 bmi should equal 20 when it's rounded to the nearest whole number.
 
